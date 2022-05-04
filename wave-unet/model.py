1b94c926f2b35c612f5653a81652ca0dc61a8dbe
mindle714 <mindle714@naver.com>
2022-05-05 04:20:14 +0900

    add multi resolution stft loss

--- wave-unet/model.py
+++ wave-unet/model.py
@@ -10,48 +10,75 @@ class waveunet(tf.keras.layers.Layer):
   def __init__(self, *args, **kwargs):
     super(waveunet, self).__init__(*args, **kwargs)
     self.layer = 8
-    self.dim = 64
+    self.dims = [64, 64, 64, 64, 128, 128, 128, 128]
     self.ksize = 5
 
   def build(self, input_shape):
     conv_opt = dict(padding='same')
 
-    self.conv_pre = conv1d(self.dim, self.ksize, **conv_opt)
+    self.conv_pre = conv1d(self.dims[0], self.ksize, **conv_opt)
     self.conv_post = conv1d(1, self.ksize, **conv_opt)
 
-    self.down_convs = [conv1d(self.dim, self.ksize,
-      strides=2, **conv_opt) for _ in range(self.layer)]    
-    self.up_convs = [conv1dtrans(self.dim, self.ksize,
-      strides=2, **conv_opt) for _ in range(self.layer)]
+    self.down_convs = list(zip(
+      [conv1d(self.dims[idx], 3,
+        strides=2, **conv_opt) for idx in range(self.layer)],
+      [conv1d(self.dims[idx], self.ksize,
+        strides=1, **conv_opt) for idx in range(self.layer)]))
 
-    self.conv_mid = conv1d(self.dim, self.ksize, **conv_opt)
+    self.up_convs = list(zip(
+      [conv1dtrans(self.dims[::-1][idx], 3,
+        strides=2, **conv_opt) for idx in range(self.layer)],
+      [conv1d(self.dims[::-1][idx], self.ksize,
+        strides=1, **conv_opt) for idx in range(self.layer)]))
+
+    self.conv_mid = conv1d(self.dims[-1], self.ksize, **conv_opt)
 
   def call(self, inputs, training=None):
     x, ref = inputs
-
-    if ref is not None:
-      ref = tf_expd(ref, -1)
 
     x = tf_expd(x, -1)
     x = tf.nn.relu(self.conv_pre(x))
 
     encs = []
-    for down_conv in self.down_convs:
-      x = tf.nn.relu(down_conv(x))
+    for down_conv, conv in self.down_convs:
+      x = tf.nn.relu(conv(x))
       encs.append(x)
+      x = tf.nn.relu(down_conv(x))
 
     x = self.conv_mid(x)
 
     encs = encs[::-1]
-    for enc, up_conv in zip(encs, self.up_convs):
-      x = tf.concat([x, enc], -1)
+    for enc, (up_conv, conv) in zip(encs, self.up_convs):
       x = tf.nn.relu(up_conv(x))
+      x = tf.concat([x, enc], -1)
+      x = tf.nn.relu(conv(x))
     
     x = self.conv_post(x)
     x = tf.math.tanh(x)
+    x = tf.squeeze(x, -1)
 
     if ref is not None:
       samp_loss = tf.math.reduce_mean((x - ref) ** 2)
-      def stft_loss(
+
+      def stft_loss(x, ref, frame_length, frame_step, fft_length):
+        stft_opt = dict(frame_length=frame_length,
+          frame_step=frame_step, fft_length=fft_length)
+        mag_x = tf.math.abs(stft(x, **stft_opt))
+        mag_ref = tf.math.abs(stft(ref, **stft_opt))
+
+        fro_opt = dict(axis=(-2, -1), ord='fro')
+        sc_loss = tf.norm(mag_x - mag_ref, **fro_opt) / (tf.norm(mag_x, **fro_opt) + 1e-9)
+        sc_loss = tf.reduce_mean(sc_loss)
+
+        mag_loss = tf.math.log(mag_x + 1e-9) - tf.math.log(mag_ref + 1e-9)
+        mag_loss = tf.reduce_mean(tf.math.abs(mag_loss))
+
+        return sc_loss + mag_loss
+
+      spec_loss = stft_loss(x, ref, 25, 5, 1024)
+      spec_loss += stft_loss(x, ref, 50, 10, 2048)
+      spec_loss += stft_loss(x, ref, 10, 2, 512)
+
+      return samp_loss + spec_loss
 
     return x
--- wave-unet/util.py
+++ wave-unet/util.py
@@ -2,25 +2,33 @@ import tensorflow as tf
 import functools
 import numpy as np
 
-def mel_filterbank(
-    pcm, sr=16000,
-    frame_length=25, frame_step=10, fft_length=None,
-    window_fn=functools.partial(tf.signal.hann_window, periodic=True),
-    lower_edge_hertz=80.0, upper_edge_hertz=7600.0, num_mel_bins=24,
-    log_noise_floor=1e-3):
-
+def stft(
+  pcm, sr=16000,
+  frame_length=25, frame_step=10, fft_length=None,
+  window_fn=functools.partial(tf.signal.hann_window, periodic=True)):
+  
   frame_length = int(frame_length * sr / 1e3)
   frame_step = int(frame_step * sr / 1e3)
   if fft_length is None:
     fft_length = int(2**(np.ceil(np.log2(frame_length))))
 
-  stfts = tf.signal.stft(
-      pcm,
+  return tf.signal.stft(
+    pcm, frame_length=frame_length, frame_step=frame_step,
+    fft_length=fft_length, window_fn=window_fn, pad_end=True)
+
+def mel_filterbank(
+  pcm, sr=16000,
+  frame_length=25, frame_step=10, fft_length=None,
+  window_fn=functools.partial(tf.signal.hann_window, periodic=True),
+  lower_edge_hertz=80.0, upper_edge_hertz=7600.0, num_mel_bins=24,
+  log_noise_floor=1e-3):
+
+  stfts = stft(
+      pcm, sr=sr,
       frame_length=frame_length,
       frame_step=frame_step,
       fft_length=fft_length,
-      window_fn=window_fn,
-      pad_end=True)
+      window_fn=window_fn)
 
   # An energy spectrogram is the magnitude of the complex-valued STFT.
   # A float32 Tensor of shape [batch_size, ?, 257].
