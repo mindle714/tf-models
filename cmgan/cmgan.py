6bfa3a3b960f78124cdca50ce34b48f4fe04cd68
mindle714 <mindle714@naver.com>
2022-12-05 00:15:54 +0900

    integrity checked before power_uncompress

--- cmgan/cmgan.py
+++ cmgan/cmgan.py
@@ -47,22 +47,62 @@ class self_attn(tf.keras.layers.Layer):
     return ctx
 
 class attention(tf.keras.layers.Layer):
-  def __init__(self, *args, **kwargs):
+  def __init__(self, heads = 8, hdim = 64, *args, **kwargs):
     super(attention, self).__init__()
+
+    self.heads = heads
+    self.hdim = hdim
+    self.max_pemb = 512
 
   def build(self, input_shape):
     dim = input_shape[0][-1]
-    self.self_attn = self_attn()
+
+    in_dim = self.hdim * self.heads
+    
+    self.q = tf.keras.layers.Dense(in_dim, use_bias=False)
+    self.kv = tf.keras.layers.Dense(in_dim * 2, use_bias=False)
     self.out = tf.keras.layers.Dense(dim, use_bias=True)
-    self.lnorm = lnorm(affine=True, eps=1e-12)
+
+    self.rel_pemb = tf.keras.layers.Embedding(2 * self.max_pemb + 1, self.hdim)
+    self.drop = tf.keras.layers.Dropout(rate = 0.2)
   
   def call(self, inputs, training=None):
-    _x, attn_mask = inputs
+    x, attn_mask = inputs
 
-    x = self.self_attn((_x, attn_mask))
-    x = self.out(x) + _x
-    x = self.lnorm(x)
+    q = self.q(x)
+    k, v = tf.split(self.kv(x), 2, axis=-1)
+   
+    def reshape(e):
+      e = tf.reshape(e,
+        tf.concat([tf.shape(x)[:2], [self.heads, self.hdim]], 0))
+      e = tf.transpose(e, [0, 2, 1, 3])
+      return e
 
+    q = reshape(q)
+    k = reshape(k)
+    v = reshape(v)
+    
+    scale = self.hdim ** -0.5
+    attn_score = tf.linalg.matmul(q, k, transpose_b=True)
+    attn_score *= scale
+
+    seq = tf.range(tf.shape(x)[-2])
+    dist = tf_expd(seq, -1) - tf_expd(seq, 0)
+    dist = tf.clip_by_value(dist, - self.max_pemb, self.max_pemb) + self.max_pemb
+    rel_pemb = self.rel_pemb(dist)
+    pos_attn = tf.linalg.matmul(tf_expd(q, -2), tf.transpose(rel_pemb, [0, 2, 1]))
+    pos_attn = tf.squeeze(pos_attn, -2) * scale
+    attn_score += pos_attn
+
+    attn_probs = tf.nn.softmax(attn_score, -1)
+    ctx = tf.linalg.matmul(attn_probs, v)
+    ctx = tf.transpose(ctx, [0, 2, 1, 3])
+    ctx = tf.reshape(ctx,
+      tf.concat([tf.shape(ctx)[:-2], [-1]], 0))
+
+    x = self.out(ctx)
+    x = self.drop(x)
+ 
     return x
 
 class fforward(tf.keras.layers.Layer):
@@ -71,10 +111,10 @@ class fforward(tf.keras.layers.Layer):
     self.mult = mult
 
   def build(self, input_shape):
-    dim = input_shape[0][-1]
+    dim = input_shape[-1]
 
     self.ff1 = tf.keras.layers.Dense(dim * self.mult)
-    self.drop = tf.keras.layers.Dropout()
+    self.drop = tf.keras.layers.Dropout(rate = 0.2)
     self.ff2 = tf.keras.layers.Dense(dim)
   
   def call(self, inputs, training=None):
@@ -91,43 +131,55 @@ class fforward(tf.keras.layers.Layer):
 class conformerconv(tf.keras.layers.Layer):
   def __init__(self, *args, **kwargs):
     super(conformerconv, self).__init__()
+
     self.exp_factor = 2
+    self.ksize = 31
 
   def build(self, input_shape):
     dim = input_shape[-1]
 
     self.lnorm = lnorm()
-    self.conv = conv1d(dim * exp_factor * 2, 1)
-    self.glu = glu()
-    self.dconv = depthconv1d()
-    self.bnorm = tf.keras.layers.BatchNormalization()
+    self.conv = conv1d(dim * self.exp_factor * 2, 1)
+    self.dconv = depthconv1d(self.ksize)
+    self.bnorm = tf.keras.layers.BatchNormalization(epsilon = 1e-5, momentum = 0.9)
     self.conv_2 = conv1d(dim, 1)
-    self.drop = tf.keras.layers.Dropout()
+    self.drop = tf.keras.layers.Dropout(rate = 0.)
   
   def call(self, inputs, training=None):
     x = inputs
 
     x = self.lnorm(x)
     x = self.conv(x)
-    x = self.glu(x)
+    x, gate = tf.split(x, 2, axis=-1)
+    x = x * tf.math.sigmoid(gate)
     x = self.dconv(x)
     x = self.bnorm(x)
+    x = tf.keras.activations.swish(x)
     x = self.conv_2(x)
     x = self.drop(x)
 
     return x
 
 class conformer(tf.keras.layers.Layer):
-  def __init__(self, *args, **kwargs):
+  def __init__(self, 
+               heads = 8, hdim = 64, ff_mult = 4,
+               conv_exp_factor = 2, ksize = 31,
+               *args, **kwargs):
     super(conformer, self).__init__()
 
-  def build(self, input_shape):
-    dim = input_shape[0][-1]
+    self.hdim = hdim
+    self.heads = heads
+    self.ff_mult = ff_mult
+    self.conv_exp_factor = conv_exp_factor
+    self.ksize = ksize
 
-    self.ff1 = fforward()
-    self.attn = attention()
+  def build(self, input_shape):
+    dim = input_shape[-1]
+
+    self.ff1 = fforward(mult = self.ff_mult)
+    self.attn = attention(heads = self.heads, hdim = self.hdim)
     self.conv = conformerconv()
-    self.ff2 = fforward()
+    self.ff2 = fforward(mult = self.ff_mult)
 
     self.attn_norm = lnorm()
     self.ff1_norm = lnorm()
@@ -136,28 +188,41 @@ class conformer(tf.keras.layers.Layer):
     self.norm = lnorm()
   
   def call(self, inputs, training=None):
-    x, mask = inputs
+    x = inputs
 
-    x = self.ff1_norm(self.ff1(x)) * 0.5 + x
-    x = self.attn_norm(self.attn((x, mask))) + x
+    x = self.ff1(self.ff1_norm(x)) * 0.5 + x
+    x = self.attn((self.attn_norm(x), None)) + x
     x = self.conv(x) + x
-    x = self.ff2_norm(self.ff2(x)) * 0.5 + x
+    x = self.ff2(self.ff2_norm(x)) * 0.5 + x
     x = self.norm(x)
 
     return x
 
 class tscb(tf.keras.layers.Layer):
-  def __init__(self, *args, **kwargs):
+  def __init__(self, channel = 64, *args, **kwargs):
     super(tscb, self).__init__()
+    self.channel = 64
 
   def build(self, input_shape):
     dim = input_shape[-1]
-    self.t_conformer = conformer()
-    self.f_conformer = conformer()
+    self.t_conformer = conformer(heads = 4, hdim = self.channel // 4)
+    self.f_conformer = conformer(heads = 4, hdim = self.channel // 4)
   
   def call(self, inputs, training=None):
     x = inputs
 
+    x = tf.transpose(x, [0, 2, 1, 3])
+    x_shape = tf.shape(x)
+    x = tf.reshape(x, tf.concat([[-1], tf.shape(x)[2:]], -1))
+    x = self.t_conformer(x) + x
+
+    x = tf.reshape(x, x_shape)
+    x = tf.transpose(x, [0, 2, 1, 3])
+    x_shape = tf.shape(x)
+    x = tf.reshape(x, tf.concat([[-1], tf.shape(x)[2:]], -1))
+    x = self.f_conformer(x) + x
+
+    x = tf.reshape(x, x_shape)
     return x
 
 class dildense(tf.keras.layers.Layer):
@@ -227,6 +292,85 @@ class dencoder(tf.keras.layers.Layer):
 
     return x
 
+class spconvtrans2d(tf.keras.layers.Layer):
+  def __init__(self, dim, ksize, r = 1, *args, **kwargs):
+    super(spconvtrans2d, self).__init__()
+
+    self.dim = dim
+    self.ksize = ksize
+    self.r = r
+
+  def build(self, input_shape):
+    self.conv = conv2d(self.dim * self.r, self.ksize, strides=(1,1))
+  
+  def call(self, inputs, training=None):
+    x = inputs
+
+    x = tf.pad(x, tf.constant(
+      [[0, 0], [0, 0], [1, 1], [0, 0]]))
+    x = self.conv(x)
+
+    dim = tf.shape(x)[-1]
+    x = tf.reshape(x, tf.concat(
+      [tf.shape(x)[:3], [self.r, dim // self.r]], -1))
+    x = tf.transpose(x, [0, 1, 4, 2, 3]) # [batch, h, dim, w, r]
+    x = tf.reshape(x, tf.concat(
+      [tf.shape(x)[:3], [-1]], -1)) # [batch, h, dim, wr]
+    x = tf.transpose(x, [0, 1, 3, 2])
+
+    return x
+
+class mdecoder(tf.keras.layers.Layer):
+  def __init__(self, channel = 64, dim = 1, *args, **kwargs):
+    super(mdecoder, self).__init__()
+    self.dim = dim
+    self.channel = channel
+
+  def build(self, input_shape):
+    self.dildense = dildense()
+    self.subpx = spconvtrans2d(self.channel, (1, 3), 2)
+    self.conv = conv2d(self.dim, (1, 2))
+    self.norm = inorm2d()
+    self.prelu = tf.keras.layers.PReLU(shared_axes=[1,2])
+    self.conv_2 = conv2d(self.dim, (1, 1))
+    self.prelu_2 = tf.keras.layers.PReLU(shared_axes=[1],
+      alpha_initializer=tf.initializers.constant(-0.25))
+  
+  def call(self, inputs, training=None):
+    x = inputs
+
+    x = self.dildense(x)
+    x = self.subpx(x)
+    x = self.conv(x)
+    x = self.prelu(self.norm(x))
+    x = self.conv_2(x)
+    x = self.prelu_2(x)
+
+    return x
+
+class cdecoder(tf.keras.layers.Layer):
+  def __init__(self, channel = 64, dim = 2, *args, **kwargs):
+    super(cdecoder, self).__init__()
+    self.dim = dim
+    self.channel = channel
+
+  def build(self, input_shape):
+    self.dildense = dildense()
+    self.subpx = spconvtrans2d(self.channel, (1, 3), 2)
+    self.conv = conv2d(self.dim, (1, 2))
+    self.norm = inorm2d()
+    self.prelu = tf.keras.layers.PReLU(shared_axes=[1,2])
+  
+  def call(self, inputs, training=None):
+    x = inputs
+
+    x = self.dildense(x)
+    x = self.subpx(x)
+    x = self.prelu(self.norm(x))
+    x = self.conv(x)
+
+    return x
+
 class tscnet(tf.keras.layers.Layer):
   def __init__(self, n_fft, hop_len, *args, **kwargs):
     super(tscnet, self).__init__()
@@ -236,6 +380,8 @@ class tscnet(tf.keras.layers.Layer):
   def build(self, input_shape):
     self.denc = dencoder()
     self.tscbs = [tscb() for _ in range(4)]
+    self.mdecoder = mdecoder()
+    self.cdecoder = cdecoder()
   
   def call(self, inputs, training=None):
     x = inputs
@@ -249,7 +395,14 @@ class tscnet(tf.keras.layers.Layer):
     for tscb in self.tscbs:
       x = tscb(x)
 
-    return x
+    mask = self.mdecoder(x)
+    out_mag = tf.squeeze(mask, -1) * mag
+
+    x = self.cdecoder(x)
+    x_r = out_mag * tf.cos(phase) + x[..., 0]
+    x_i = out_mag * tf.sin(phase) + x[..., 1]
+
+    return x_r, x_i
 
 class cmgan(tf.keras.layers.Layer):
   def __init__(self, *args, **kwargs):
@@ -276,6 +429,13 @@ class cmgan(tf.keras.layers.Layer):
     phase = tf.math.angle(x)
     mag **= 0.3
     return tf.complex(mag * tf.cos(phase), mag * tf.sin(phase))
+  
+  def power_uncompress(self, x_r, x_i):
+    x = tf.complex(x_r, x_i)
+    mag = tf.math.abs(x)
+    phase = tf.math.angle(x)
+    mag **= (1. / 0.3)
+    return tf.complex(mag * tf.cos(phase), mag * tf.sin(phase))
 
   def call(self, inputs, training=None):
     if isinstance(inputs, tuple):
@@ -295,12 +455,19 @@ class cmgan(tf.keras.layers.Layer):
     tot_len = tf.cast(tf.math.ceil(x_len / 100), tf.int32) * 100
     pad_len = tot_len - x_len
     x = tf.concat([x, x[:, :pad_len]], -1)
+
     x = tf.pad(x, tf.constant(
       [[0, 0], [self.n_fft//2, self.n_fft//2]]), mode='reflect')
-
     x = tf.signal.stft(x, window_fn=tf.signal.hamming_window, 
       frame_length=self.n_fft, frame_step=self.hop_len, fft_length=self.n_fft)
+    
     x = self.power_compress(x)
+    x_r, x_i = self.tscnet(x)
+    x = self.power_uncompress(x_r, x_i)
 
-    x = self.tscnet(x)
+    x = tf.signal.inverse_stft(x, window_fn=tf.signal.hamming_window,
+      frame_length=self.n_fft, frame_step=self.hop_len, fft_length=self.n_fft)
+    x /= c
+    x = x[..., self.n_fft//2:self.n_fft//2+x_len]
+
     return x
--- cmgan/test_cmgan.py
+++ cmgan/test_cmgan.py
@@ -15,128 +15,195 @@ _in = np.reshape(pcm, [1, -1])
 _tmp = model(_in)
 print(_tmp)
 
-w = m['dense_encoder.conv_1.0.weight'].cpu().numpy()
-b = m['dense_encoder.conv_1.0.bias'].cpu().numpy()
-model.tscnet.denc.conv_1.set_weights([w.transpose([2, 3, 1, 0]), b])
-
-w = m['dense_encoder.conv_1.1.weight'].cpu().numpy()
-b = m['dense_encoder.conv_1.1.bias'].cpu().numpy()
-model.tscnet.denc.inorm2d.gamma.assign(w)
-model.tscnet.denc.inorm2d.beta.assign(b)
-
-w = m['dense_encoder.conv_1.2.weight'].cpu().numpy()
-model.tscnet.denc.prelu.set_weights([w.reshape([1, 1, -1])])
-
-prefix = 'dense_encoder.dilated_dense'
-for i in range(len(model.tscnet.denc.dildense.convs)):
-  w = m['{}.conv{}.weight'.format(prefix, i+1)].cpu().numpy()
-  b = m['{}.conv{}.bias'.format(prefix, i+1)].cpu().numpy()
-  model.tscnet.denc.dildense.convs[i].set_weights([w.transpose([2, 3, 1, 0]), b])
+def load_dildense(prefix, _model):
+  for i in range(len(_model.convs)):
+    w = m['{}.conv{}.weight'.format(prefix, i+1)].cpu().numpy()
+    b = m['{}.conv{}.bias'.format(prefix, i+1)].cpu().numpy()
+    _model.convs[i].set_weights([w.transpose([2, 3, 1, 0]), b])
   
-  w = m['{}.norm{}.weight'.format(prefix, i+1)].cpu().numpy()
-  b = m['{}.norm{}.bias'.format(prefix, i+1)].cpu().numpy()
-  model.tscnet.denc.dildense.norms[i].gamma.assign(w)
-  model.tscnet.denc.dildense.norms[i].beta.assign(b)
+    w = m['{}.norm{}.weight'.format(prefix, i+1)].cpu().numpy()
+    b = m['{}.norm{}.bias'.format(prefix, i+1)].cpu().numpy()
+    _model.norms[i].gamma.assign(w)
+    _model.norms[i].beta.assign(b)
  
-  w = m['{}.prelu{}.weight'.format(prefix, i+1)].cpu().numpy()
-  model.tscnet.denc.dildense.prelus[i].set_weights([w.reshape([1, 1, -1])])
+    w = m['{}.prelu{}.weight'.format(prefix, i+1)].cpu().numpy()
+    _model.prelus[i].set_weights([w.reshape([1, 1, -1])])
 
-w = m['dense_encoder.conv_2.0.weight'].cpu().numpy()
-b = m['dense_encoder.conv_2.0.bias'].cpu().numpy()
-model.tscnet.denc.conv_2.set_weights([w.transpose([2, 3, 1, 0]), b])
+def load_dencoder(prefix, _model):
+  w = m['{}.conv_1.0.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv_1.0.bias'.format(prefix)].cpu().numpy()
+  _model.conv_1.set_weights([w.transpose([2, 3, 1, 0]), b])
 
-w = m['dense_encoder.conv_2.1.weight'].cpu().numpy()
-b = m['dense_encoder.conv_2.1.bias'].cpu().numpy()
-model.tscnet.denc.inorm2d_2.gamma.assign(w)
-model.tscnet.denc.inorm2d_2.beta.assign(b)
+  w = m['{}.conv_1.1.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv_1.1.bias'.format(prefix)].cpu().numpy()
+  _model.inorm2d.gamma.assign(w)
+  _model.inorm2d.beta.assign(b)
 
-w = m['dense_encoder.conv_2.2.weight'].cpu().numpy()
-model.tscnet.denc.prelu_2.set_weights([w.reshape([1, 1, -1])])
+  w = m['{}.conv_1.2.weight'.format(prefix)].cpu().numpy()
+  _model.prelu.set_weights([w.reshape([1, 1, -1])])
 
-print(model(_in))
-import sys
-sys.exit()
+  load_dildense('{}.dilated_dense'.format(prefix), _model.dildense)
 
-def load_norm(prefix, e):
-  w = m['{}.weight'.format(prefix, i)].cpu().numpy()
-  b = m['{}.bias'.format(prefix, i)].cpu().numpy()
-  e.gamma.assign(w)
-  e.beta.assign(b)
+  w = m['{}.conv_2.0.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv_2.0.bias'.format(prefix)].cpu().numpy()
+  _model.conv_2.set_weights([w.transpose([2, 3, 1, 0]), b])
 
-def load_affine(prefix, e):
-  w = m['{}.weight'.format(prefix)]
-  bname = '{}.bias'.format(prefix)
-  if bname in m:
-    b = m[bname]
-    e.set_weights([w.transpose(1,0).cpu().numpy(), b.cpu().numpy()])
-  else:
-    e.set_weights([w.transpose(1,0).cpu().numpy()])
+  w = m['{}.conv_2.1.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv_2.1.bias'.format(prefix)].cpu().numpy()
+  _model.inorm2d_2.gamma.assign(w)
+  _model.inorm2d_2.beta.assign(b)
 
-def load_conv(prefix, e):
-  w = m['{}.weight'.format(prefix)]
-  bname = '{}.bias'.format(prefix)
-  if bname in m:
-    b = m[bname]
-    e.set_weights([w.transpose(2,0).cpu().numpy(), b.cpu().numpy()])
-  else:
-    e.set_weights([w.transpose(2,0).cpu().numpy()])
+  w = m['{}.conv_2.2.weight'.format(prefix)].cpu().numpy()
+  _model.prelu_2.set_weights([w.reshape([1, 1, -1])])
 
-for i, conv in enumerate(model.wav2vec2.fe.conv_layers):
-  prefix = 'wav2vec2.feature_extractor.conv_layers'
-  load_conv('{}.{}.conv'.format(prefix, i), conv.conv)
-  if i == 0:
-    load_norm('{}.{}.layer_norm'.format(prefix, i), conv.norm)
+def load_conformer(prefix, _model):
+  w = m['{}.ff1.fn.fn.net.0.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff1.fn.fn.net.0.bias'.format(prefix)].cpu().numpy()
+  _model.ff1.ff1.set_weights([w.transpose([1, 0]), b])
 
-prefix = 'wav2vec2.feature_projection'
-load_norm('{}.layer_norm'.format(prefix), model.wav2vec2.fp.norm)
-load_affine('{}.projection'.format(prefix), model.wav2vec2.fp.proj)
+  w = m['{}.ff1.fn.fn.net.3.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff1.fn.fn.net.3.bias'.format(prefix)].cpu().numpy()
+  _model.ff1.ff2.set_weights([w.transpose([1, 0]), b])
 
-prefix = 'wav2vec2.encoder'
-w_g = m['{}.pos_conv_embed.conv.weight_g'.format(prefix)].cpu().numpy()
-w_g = np.reshape(w_g, [-1, 1, 1])
-w_v = m['{}.pos_conv_embed.conv.weight_v'.format(prefix)].transpose(2,0).cpu().numpy()
-w = tf.nn.l2_normalize(w_v, axis=[1,2]) * w_g
-b = m['{}.pos_conv_embed.conv.bias'.format(prefix)].cpu().numpy()
-model.wav2vec2.enc.emb.conv.set_weights([w, b])
-load_norm('{}.layer_norm'.format(prefix), model.wav2vec2.enc.norm)
+  w = m['{}.ff1.fn.norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff1.fn.norm.bias'.format(prefix)].cpu().numpy()
+  _model.ff1_norm.gamma.assign(w)
+  _model.ff1_norm.beta.assign(b)
 
-for i, layer in enumerate(model.wav2vec2.enc.layers):
-  prefix = 'wav2vec2.encoder.layers.{}'.format(i)
-  load_affine('{}.attention.q_proj'.format(prefix), layer.atten.q_proj)
-  load_affine('{}.attention.k_proj'.format(prefix), layer.atten.k_proj)
-  load_affine('{}.attention.v_proj'.format(prefix), layer.atten.v_proj)
-  load_affine('{}.attention.out_proj'.format(prefix), layer.atten.out_proj)
- 
-  load_affine('{}.feed_forward.intermediate_dense'.format(prefix), layer.feed.in_dense)
-  load_affine('{}.feed_forward.output_dense'.format(prefix), layer.feed.out_dense)
-  
-  load_norm('{}.layer_norm'.format(prefix), layer.norm)
-  load_norm('{}.final_layer_norm'.format(prefix), layer.out_norm)
+  w = m['{}.attn.norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.attn.norm.bias'.format(prefix)].cpu().numpy()
+  _model.attn_norm.gamma.assign(w)
+  _model.attn_norm.beta.assign(b)
 
-#load_affine('projector', model.projector)
-#load_affine('classifier', model.classifier)
+  w = m['{}.attn.fn.to_q.weight'.format(prefix)].cpu().numpy()
+  _model.attn.q.set_weights([w.transpose([1, 0])])
+
+  w = m['{}.attn.fn.to_kv.weight'.format(prefix)].cpu().numpy()
+  _model.attn.kv.set_weights([w.transpose([1, 0])])
+
+  w = m['{}.attn.fn.rel_pos_emb.weight'.format(prefix)].cpu().numpy()
+  _model.attn.rel_pemb.set_weights([w])
+
+  w = m['{}.attn.fn.to_out.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.attn.fn.to_out.bias'.format(prefix)].cpu().numpy()
+  _model.attn.out.set_weights([w.transpose([1, 0]), b])
+
+  w = m['{}.conv.net.0.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.net.0.bias'.format(prefix)].cpu().numpy()
+  _model.conv.lnorm.gamma.assign(w)
+  _model.conv.lnorm.beta.assign(b)
+
+  w = m['{}.conv.net.2.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.net.2.bias'.format(prefix)].cpu().numpy()
+  _model.conv.conv.set_weights([w.transpose([2, 1, 0]), b])
+
+  w = m['{}.conv.net.4.conv.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.net.4.conv.bias'.format(prefix)].cpu().numpy()
+  _model.conv.dconv.set_weights([w.transpose([2, 0, 1]), b])
+
+  w = m['{}.conv.net.5.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.net.5.bias'.format(prefix)].cpu().numpy()
+  rm = m['{}.conv.net.5.running_mean'.format(prefix)].cpu().numpy()
+  rv = m['{}.conv.net.5.running_var'.format(prefix)].cpu().numpy()
+  _model.conv.bnorm.set_weights([w, b, rm, rv])
+
+  w = m['{}.conv.net.7.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.net.7.bias'.format(prefix)].cpu().numpy()
+  _model.conv.conv_2.set_weights([w.transpose([2, 1, 0]), b])
+
+  w = m['{}.ff2.fn.fn.net.0.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff2.fn.fn.net.0.bias'.format(prefix)].cpu().numpy()
+  _model.ff2.ff1.set_weights([w.transpose([1, 0]), b])
+
+  w = m['{}.ff2.fn.fn.net.3.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff2.fn.fn.net.3.bias'.format(prefix)].cpu().numpy()
+  _model.ff2.ff2.set_weights([w.transpose([1, 0]), b])
+
+  w = m['{}.ff2.fn.norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.ff2.fn.norm.bias'.format(prefix)].cpu().numpy()
+  _model.ff2_norm.gamma.assign(w)
+  _model.ff2_norm.beta.assign(b)
+
+  w = m['{}.post_norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.post_norm.bias'.format(prefix)].cpu().numpy()
+  _model.norm.gamma.assign(w)
+  _model.norm.beta.assign(b)
+
+def load_tscb(prefix, _model):
+  load_conformer('{}.time_conformer'.format(prefix), _model.t_conformer)
+  load_conformer('{}.freq_conformer'.format(prefix), _model.f_conformer)
+
+def load_spconvtrans2d(prefix, _model):
+  w = m['{}.conv.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.bias'.format(prefix)].cpu().numpy()
+  _model.conv.set_weights([w.transpose([2, 3, 1, 0]), b])
+
+def load_mdecoder(prefix, _model):
+  load_dildense('{}.dense_block'.format(prefix), _model.dildense)
+  load_spconvtrans2d('{}.sub_pixel'.format(prefix), _model.subpx)
+
+  w = m['{}.conv_1.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv_1.bias'.format(prefix)].cpu().numpy()
+  _model.conv.set_weights([w.transpose([2, 3, 1, 0]), b])
+
+  w = m['{}.norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.norm.bias'.format(prefix)].cpu().numpy()
+  _model.norm.gamma.assign(w)
+  _model.norm.beta.assign(b)
+    
+  w = m['{}.prelu.weight'.format(prefix)].cpu().numpy()
+  _model.prelu.set_weights([w.reshape([1, 1, -1])])
+
+  w = m['{}.final_conv.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.final_conv.bias'.format(prefix)].cpu().numpy()
+  _model.conv_2.set_weights([w.transpose([2, 3, 1, 0]), b])
+    
+  w = m['{}.prelu_out.weight'.format(prefix)].cpu().numpy()
+  _model.prelu_2.set_weights([w.reshape([1, -1, 1])])
+
+def load_cdecoder(prefix, _model):
+  load_dildense('{}.dense_block'.format(prefix), _model.dildense)
+  load_spconvtrans2d('{}.sub_pixel'.format(prefix), _model.subpx)
+
+  w = m['{}.conv.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.conv.bias'.format(prefix)].cpu().numpy()
+  _model.conv.set_weights([w.transpose([2, 3, 1, 0]), b])
+
+  w = m['{}.norm.weight'.format(prefix)].cpu().numpy()
+  b = m['{}.norm.bias'.format(prefix)].cpu().numpy()
+  _model.norm.gamma.assign(w)
+  _model.norm.beta.assign(b)
+    
+  w = m['{}.prelu.weight'.format(prefix)].cpu().numpy()
+  _model.prelu.set_weights([w.reshape([1, 1, -1])])
+
+def load_tscnet(prefix, _model):
+  load_dencoder('dense_encoder', _model.denc)
+  for i in range(len(_model.tscbs)):
+    load_tscb('TSCB_{}'.format(i+1), _model.tscbs[i])
+  load_mdecoder('mask_decoder', _model.mdecoder)
+  load_cdecoder('complex_decoder', _model.cdecoder)
+
+load_tscnet('', model.tscnet)
 
 print(model(_in))
 
 '''
-(venv3.7-tera) hejung@speech:~/tf-models/ssl$ python3 test_wav2vec2.py
+hejung@speech:~/tf-models/ssl$ python3 test_cmgan.py
 tf.Tensor(
-[[[-0.03795013  0.18967038  0.17773671 ...  0.22049077  0.2822556
-    0.33677945]
-  [-0.00936582  0.41093287 -0.02825753 ...  0.4727972   0.1705111
-    0.26599422]
-  [ 0.06454878  0.18859263 -0.02218433 ...  0.14479631  0.19384265
-    0.2460265 ]
-  ...
-  [-0.1849951   0.14248125 -0.14958894 ...  0.19302145  0.14552265
-    0.27958643]
-  [-0.10210146  0.41023663 -0.08990705 ...  0.0936659   0.1708027
-    0.2306513 ]
-  [ 0.00899731  0.21980184  0.18803358 ...  0.04501326  0.19920312
-    0.35360476]]], shape=(1, 49, 768), dtype=float32)
+[[-0.00904491 -0.03413181 -0.03042892 ...  0.10910118  0.1264352
+   0.14365053]], shape=(1, 33342), dtype=float32)
+tf.Tensor(
+[[-0.00291189 -0.00660475 -0.00511546 ...  0.00640525  0.00628492
+   0.00696982]], shape=(1, 33342), dtype=float32)
 
-pushd ~/s3prl/s3prl
-python3 -m pdb run_downstream.py -m train -n ExpName -k /home/hejung/wav2vec2-base/pytorch_model.bin -g /home/hejung/wav2vec2-base/config.json -d speech_commands
-(Pdb) b /home/hejung/venv3.7-tera/lib/python3.7/site-packages/transformers/models/wav2vec2/modeling_wav2vec2.py:1079
+ssh t2
+pushd ~/CMGAN/src
+(venv3.7-cmgan) hejung@speecht2:~/CMGAN/src$ CUDA_VISIBLE_DEVICES=1 python3 -m pdb evaluation.py --test_dir dummy_one/test/ --model_path best_ckpt/ckpt
+(Pdb) b 49
 (Pdb) c
+(Pdb) p est_audio
+array([-0.00224336, -0.00505821, -0.00389482, ...,  0.00409852,
+        0.0040251 ,  0.00446793], dtype=float32)
+(It seems that OLA strategy is different between torch and tensorflow)
 '''
