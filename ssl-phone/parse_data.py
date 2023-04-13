16b83597b6e3b9a5718e27eb43073f76d3d3ec6d
mindle714 <mindle714@naver.com>
2023-04-14 01:14:56 +0900

    fix frame len, move feat extractor to data parser

--- ssl-phone/parse_data.py
+++ ssl-phone/parse_data.py
@@ -12,6 +12,30 @@ def parse_func(pcm_len, txt_len):
     return tf.io.parse_single_example(ex, desc)
   return _parse_func
 
+from util import melspec
+
+def _normalize_wav_decibel(wav, target_level=-25):
+  rms = (tf.math.reduce_mean(wav**2))**0.5
+  scalar = (10 ** (target_level / 20)) / (rms + 1e-10)
+  wav = wav * scalar
+  return wav
+
+def conv_spec(e, n_fft=400, hop_len=160):
+  x = e['pcm']
+  del e['pcm']
+
+  x = _normalize_wav_decibel(x)
+  x = melspec(x, num_mel_bins=80,
+    frame_length=n_fft, frame_step=hop_len, fft_length=n_fft,
+    lower_edge_hertz=0., upper_edge_hertz=8000.)
+  e['spec'] = x
+
+  x_len = e['pcm_len']
+  del e['pcm_len']
+  e['spec_len'] = int((x_len - n_fft) / hop_len) + 1
+
+  return e
+
 def gen_train(tfrec_list, pcm_len, txt_len, batch_size=16, seed=1234, epoch=None):
   dataset = tf.data.TFRecordDataset(tfrec_list)
   dataset = dataset.shuffle(batch_size*100, seed=seed, reshuffle_each_iteration=True)
@@ -25,6 +49,7 @@ def gen_train(tfrec_list, pcm_len, txt_len, batch_size=16, seed=1234, epoch=None
     dataset = dataset.batch(batch_size, drop_remainder=True)
   else:
     dataset = dataset.batch(batch_size)
+  dataset = dataset.map(conv_spec, num_parallel_calls=tf.data.experimental.AUTOTUNE)
   dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
   return dataset
 
