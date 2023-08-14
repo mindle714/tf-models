67132c33986de1d8ee706f313afad29a23be5dc6
mindle714 <mindle714@naver.com>
2023-08-14 15:26:27 +0900

    add libri symlink

--- ssl-rand/train.py
+++ ssl-rand/train.py
@@ -42,8 +42,8 @@ if args.timit:
   if args.eval_list is None: args.eval_list = "/data/hejung/timit/test.wav.phone"
 
 else:
-  if args.save_step is None: args.save_step = 10000
-  if args.train_step is None: args.train_step = 45000
+  if args.save_step is None: args.save_step = 3000
+  if args.train_step is None: args.train_step = 30000
   if args.lr_decay_step is None: args.lr_decay_step = 4000
   if args.eval_list is None: args.eval_list = "/data/hejung/librispeech/test-clean.flac.phone"
   
