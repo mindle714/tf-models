43f4d57f82d1cdd143f16d2c594cbc45683ab2bb
mindle714 <mindle714@naver.com>
2023-08-19 00:33:58 +0900

    add fix update for high magnitude

--- ssl-rand/pstat_tera.py
+++ ssl-rand/pstat_tera.py
@@ -36,10 +36,12 @@ spec = np.zeros(1701*80)
 _in = np.reshape(spec, [1,1701,80])
 _tmp = model(_in)
 
-def print_stat(e):
+def print_stat(e, end='\n'):
   w, b = e.get_weights()
   fan_in, fan_out = _compute_fans(w.shape)
-  print("{:.6f}".format(np.var(w)), "{:.6f}".format(2/(fan_in + fan_out)))
+  print("{:.6f}".format(np.mean(w)), "{:.6f}".format(np.var(w)), "{:.6f}".format(2/(fan_in + fan_out)), end=end)
+
+print(">>>>>>random init")
 
 print_stat(model.tera.tera.fe.spec_transform)
 # TODO what about layer norm weights?
@@ -55,53 +57,66 @@ for i in range(3):
   print_stat(model.tera.tera.enc.layers[i].inter)
   print_stat(model.tera.tera.enc.layers[i].out)
 
-print("load...")
+print(">>>>>>load...")
 ckpt = tf.train.Checkpoint(model)
 ckpt.read("tera_timit.ckpt")
 
 def add_rand(e, suffix = None):
   w, b = e.get_weights()
   fan_in, fan_out = _compute_fans(w.shape)
+  '''
   if suffix is None:
     print("{:.6f}".format(np.var(w)), "{:.6f}".format(2/(fan_in + fan_out)), \
             "{:.6f}".format(np.var(w) / (2/(fan_in + fan_out))))
   else:
     print("{:.6f}".format(np.var(w)), "{:.6f}".format(2/(fan_in + fan_out)), \
             "{:.6f}".format(np.var(w) / (2/(fan_in + fan_out))), suffix)
+  '''
 
   w_flat = w.flatten()
   idxs = np.argsort(np.abs(w_flat))
-#  idxs = idxs[:int(idxs.shape[0] * 0.7)]
-#  idxs = idxs[:int(idxs.shape[0] * 0.5)]
-#  idxs = idxs[:int(idxs.shape[0] * 0.3)]
-  idxs = idxs[:int(idxs.shape[0] * 0.2)]
+  idxs_mask = idxs[:int(idxs.shape[0] * 0.7)]
+  idxs_mask_inv = idxs[int(idxs.shape[0] * 0.7):]
 
   w_mask = np.ones_like(w_flat)
-  w_mask[idxs] = 0
+  w_mask[idxs_mask] = 0
   w_mask = w_mask.reshape(w.shape)
 
   init = tf.keras.initializers.GlorotUniform(seed = 1234)
   w_rand = init(shape = w.shape)
+  w_rand = np.ones_like(w_rand) * 0#np.mean(w_flat[idxs_mask_inv])
   w_rand = w_mask * w + (1 - w_mask) * w_rand
 
   e.set_weights([w_rand, b])
 
+print_stat(model.tera.tera.fe.spec_transform, end=" -> ")
 add_rand(model.tera.tera.fe.spec_transform)
+print_stat(model.tera.tera.fe.spec_transform)
 # TODO what about layer norm weights?
 
 for i in range(3):
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.query, end=" -> ")
   add_rand(model.tera.tera.enc.layers[i].atten.self_attn.query, "query[{}]".format(i))
-  add_rand(model.tera.tera.enc.layers[i].atten.self_attn.key, "key[{}]".format(i))
-  add_rand(model.tera.tera.enc.layers[i].atten.self_attn.value, "value[{}]".format(i))
-  
-  add_rand(model.tera.tera.enc.layers[i].atten.out)
-  # TODO lnorm
-  
-  add_rand(model.tera.tera.enc.layers[i].inter)
-  add_rand(model.tera.tera.enc.layers[i].out)
-  # TODO lnorm
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.query)
 
-#ckpt.write("tera_timit_rand.ckpt")
-#ckpt.write("tera_timit_rand0_5.ckpt")
-#ckpt.write("tera_timit_rand0_3.ckpt")
-ckpt.write("tera_timit_rand0_2.ckpt")
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.key, end=" -> ")
+  add_rand(model.tera.tera.enc.layers[i].atten.self_attn.key, "key[{}]".format(i))
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.key)
+
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.value, end=" -> ")
+  add_rand(model.tera.tera.enc.layers[i].atten.self_attn.value, "value[{}]".format(i))
+  print_stat(model.tera.tera.enc.layers[i].atten.self_attn.value)
+  
+  print_stat(model.tera.tera.enc.layers[i].atten.out, end=" -> ")
+  add_rand(model.tera.tera.enc.layers[i].atten.out)
+  print_stat(model.tera.tera.enc.layers[i].atten.out)
+  # TODO lnorm
+  
+  print_stat(model.tera.tera.enc.layers[i].inter, end=" -> ")
+  add_rand(model.tera.tera.enc.layers[i].inter)
+  print_stat(model.tera.tera.enc.layers[i].inter)
+
+  print_stat(model.tera.tera.enc.layers[i].out, end=" -> ")
+  add_rand(model.tera.tera.enc.layers[i].out)
+  print_stat(model.tera.tera.enc.layers[i].out)
+  # TODO lnorm
--- ssl-rand/train.py
+++ ssl-rand/train.py
@@ -26,6 +26,7 @@ parser.add_argument("--lr-decay-rate", type=float, required=False, default=0.96)
 parser.add_argument("--lr-decay-step", type=float, required=False, default=None)
 parser.add_argument("--val-lr-update", type=float, required=False, default=3) 
 parser.add_argument("--ssl-rand", type=float, required=False, default=None)
+parser.add_argument("--ssl-fix-step", type=int, required=False, default=0)
 parser.add_argument("--output", type=str, required=True) 
 parser.add_argument("--timit", action='store_true')
 parser.add_argument("--speech-command", action='store_true')
@@ -209,24 +210,23 @@ def run_step(step, spec, txt,
     grads = tape.gradient(loss, weights)
     grads, _ = tf.clip_by_global_norm(grads, 5.)
             
-    if args.accum_step == 1:
-      opt.apply_gradients(zip(grads, weights))
-            
-    else:
-      for idx, grad in enumerate(grads):
-        if grad is None: continue
+    for idx, grad in enumerate(grads):
+      if grad is None: continue
+      if step < args.ssl_fix_step:
+        accum_grads[idx].assign_add(grad * grad_mask[idx])
+      else:
         accum_grads[idx].assign_add(grad)
 
-      if not accum:
-        for idx, grad in enumerate(grads):
-          if grad is None: continue
-          accum_grads[idx].assign(accum_grads[idx]/args.accum_step)
+    if not accum:
+      for idx, grad in enumerate(grads):
+        if grad is None: continue
+        accum_grads[idx].assign(accum_grads[idx]/args.accum_step)
 
-        opt.apply_gradients(zip(accum_grads, weights))
+      opt.apply_gradients(zip(accum_grads, weights))
                 
-        for idx, grad in enumerate(grads):
-          if grad is None: continue
-          accum_grads[idx].assign(tf.zeros_like(grad))
+      for idx, grad in enumerate(grads):
+        if grad is None: continue
+        accum_grads[idx].assign(tf.zeros_like(grad))
 
   return loss
 
@@ -315,23 +315,29 @@ if args.warm_start is not None:
       opt.set_weights(opt_weight)
 
   if args.ssl_rand is not None:
+    grad_mask_dict = {e.name:tf.ones_like(e) for e in m.trainable_weights}
+
     def add_rand(e):
       w, b = e.get_weights()
 
       w_flat = w.flatten()
       idxs = np.argsort(np.abs(w_flat))
-      idxs = idxs[:int(idxs.shape[0] * args.ssl_rand)]
+      idxs_mask = idxs[:int(idxs.shape[0] * args.ssl_rand)]
+      idxs_mask_inv = idxs[int(idxs.shape[0] * args.ssl_rand):]
 
       w_mask = np.ones_like(w_flat)
-      w_mask[idxs] = 0
+      w_mask[idxs_mask] = 0
       w_mask = w_mask.reshape(w.shape)
 
       init = tf.keras.initializers.GlorotUniform(seed = seed)
       w_rand = init(shape = w.shape)
-      #w_rand = w_mask * w + (1 - w_mask) * w_rand
-      w_rand = w_mask * w
+      #w_rand = np.ones_like(w) * np.mean(w_flat[idxs_mask_inv])
+      w_rand = w_mask * w #+ (1 - w_mask) * w_rand
 
       e.set_weights([w_rand, b])
+
+      w_name = e.trainable_weights[0].name
+      grad_mask_dict[w_name] *= (1 - w_mask)
 
     add_rand(m.tera.tera.fe.spec_transform)
     # TODO what about layer norm weights?
@@ -347,6 +353,12 @@ if args.warm_start is not None:
       add_rand(m.tera.tera.enc.layers[i].inter)
       add_rand(m.tera.tera.enc.layers[i].out)
       # TODO lnorm
+
+    grad_mask = [tf.ones_like(e) for e in m.trainable_weights]
+    for idx, w in enumerate(grad_mask):
+      w_name = m.trainable_weights[idx].name
+      if w_name in grad_mask_dict:
+        grad_mask[idx] = grad_mask_dict[w_name]
 
 _traced_begin = 2; _traced_cnt = 10
 
