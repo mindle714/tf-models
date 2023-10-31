0375e7eea4f576d0f812ec703e943748129b5e3e
mindle714 <mindle714@naver.com>
2023-10-31 15:16:23 +0900

    start adding t5 decoder

--- t5/conv_t5.py
+++ t5/conv_t5.py
@@ -2,35 +2,54 @@ import tensorflow as tf
 from tensorflow.python.training import py_checkpoint_reader
 
 reader     = py_checkpoint_reader.NewCheckpointReader(
-  "/home/hejung/text-to-text-transfer-transformer/tmp/models/small/model.ckpt-1025000")
+  "/home/hejung/text-to-text-transfer-transformer/tmp/models/small/model.ckpt-1000000")
 
 dtype_map  = reader.get_variable_to_dtype_map()
 shape_map  = reader.get_variable_to_shape_map()
 
-state_dict = { v: reader.get_tensor(v) for v in shape_map}
+state_dict = {v: reader.get_tensor(v) for v in shape_map}
 
 import tfive
 model = tfive.t5()
 
 import numpy as np
-_in = np.array([22377, 822, 10, 213, 19, 8, 10283, 13767, 1069, 58, 1])
+#_in = np.array([22377, 822, 10, 213, 19, 8, 10283, 13767, 1069, 58, 1])
+'''
+_in = np.array([22377, 822, 10, 113, 751, 8, 296, 3, 7, 22563, 49, 10183, 
+    16, 164, 1412, 58, 1, 22377, 822, 10, 2170, 30, 16022,
+    1914, 10247, 6, 125, 19, 8, 244, 4350, 13, 3, 17, 208, 31, 7, 2472, 3, 
+    18118, 6, 113, 5330, 263, 112, 10393, 57, 16069, 30, 8, 12366, 9, 107, 
+    1369, 89, 60, 63, 504, 58, 1, 22377, 822, 10, 125, 19, 8, 564, 13, 8, 
+    1227, 9, 25863, 3946, 16, 8438, 1273, 84, 19, 2681, 147, 3, 9, 2068, 
+    3, 29, 31, 12, 1835, 35, 165, 30637, 6, 3, 15, 5, 122, 5, 16, 8, 1448, 
+    3, 31, 7, 15, 2, 127, 58, 1])
+'''
+_in = np.array([22377, 822, 10, 113, 1944, 528, 7, 7, 2617, 16, 388, 
+    45, 2983, 63, 4033, 58, 1]) 
+_in_len = np.array([_in.shape[0]])
 _in = np.concatenate([_in, np.zeros(128 - _in.shape[0], dtype=_in.dtype)])
 _in = _in.reshape((1, -1))
 assert _in.shape == (1, 128)
-_in_len = np.array([11])
 
-_ = model((_in, _in_len))
+_out = np.array([0, 108, 3496, 26, 3, 189, 127, 6992]) 
+_out_len = np.array([_out.shape[0]])
+_out = np.concatenate([_out, np.zeros(32 - _out.shape[0], dtype=_out.dtype)])
+_out = _out.reshape((1, -1))
+assert _out.shape == (1, 32)
+
+_ = model((_in, _in_len, _out, _out_len))
 
 emb = state_dict['shared/embedding']
 model.embed.assign(emb)
 
+# encoder
 rel_bias = state_dict['encoder/block_000/layer_000/SelfAttention/relative_attention_bias']
-model.rel_bias.assign(rel_bias)
+model.encoder.rel_bias.assign(rel_bias)
 
-assert len(model.sublayers) == 12
-for idx in range(len(model.sublayers) // 2):
+assert len(model.encoder.sublayers) == 12
+for idx in range(len(model.encoder.sublayers) // 2):
   prefix = 'encoder/block_{:03d}/layer_000'.format(idx)
-  sublayer = model.sublayers[2 * idx]
+  sublayer = model.encoder.sublayers[2 * idx]
 
   scale = state_dict['{}/layer_norm/scale'.format(prefix)]
   sublayer.layer_norm.scale.assign(scale)
@@ -45,7 +64,7 @@ for idx in range(len(model.sublayers) // 2):
   sublayer.layer.out.set_weights([out])
 
   prefix = 'encoder/block_{:03d}/layer_001'.format(idx)
-  sublayer = model.sublayers[2 * idx + 1]
+  sublayer = model.encoder.sublayers[2 * idx + 1]
 
   scale = state_dict['{}/layer_norm/scale'.format(prefix)]
   sublayer.layer_norm.scale.assign(scale)
@@ -56,7 +75,60 @@ for idx in range(len(model.sublayers) // 2):
   sublayer.layer.wo.set_weights([wo])
   
 scale = state_dict['encoder/final_layer_norm/scale']
-model.layer_norm.scale.assign(scale)
+model.encoder.layer_norm.scale.assign(scale)
 
-out = model((_in, _in_len))
+# decoder
+rel_bias = state_dict['decoder/block_000/layer_000/SelfAttention/relative_attention_bias']
+model.decoder.rel_bias.assign(rel_bias)
+
+assert len(model.decoder.sublayers) == 18
+for idx in range(len(model.decoder.sublayers) // 3):
+  # SelfAttention
+  prefix = 'decoder/block_{:03d}/layer_000'.format(idx)
+  sublayer = model.decoder.sublayers[3 * idx]
+
+  scale = state_dict['{}/layer_norm/scale'.format(prefix)]
+  sublayer.layer_norm.scale.assign(scale)
+
+  q = state_dict['{}/SelfAttention/q'.format(prefix)]
+  sublayer.layer.q.assign(q)
+  k = state_dict['{}/SelfAttention/k'.format(prefix)]
+  sublayer.layer.k.set_weights([k])
+  v = state_dict['{}/SelfAttention/v'.format(prefix)]
+  sublayer.layer.v.set_weights([v])
+  out = state_dict['{}/SelfAttention/o'.format(prefix)]
+  sublayer.layer.out.set_weights([out])
+
+  # EncDecAttention
+  prefix = 'decoder/block_{:03d}/layer_001'.format(idx)
+  sublayer = model.decoder.sublayers[3 * idx + 1]
+
+  scale = state_dict['{}/layer_norm/scale'.format(prefix)]
+  sublayer.layer_norm.scale.assign(scale)
+
+  q = state_dict['{}/EncDecAttention/q'.format(prefix)]
+  sublayer.layer.q.assign(q)
+  k = state_dict['{}/EncDecAttention/k'.format(prefix)]
+  sublayer.layer.k.set_weights([k])
+  v = state_dict['{}/EncDecAttention/v'.format(prefix)]
+  sublayer.layer.v.set_weights([v])
+  out = state_dict['{}/EncDecAttention/o'.format(prefix)]
+  sublayer.layer.out.set_weights([out])
+
+  # DenseReluDense
+  prefix = 'decoder/block_{:03d}/layer_002'.format(idx)
+  sublayer = model.decoder.sublayers[3 * idx + 2]
+
+  scale = state_dict['{}/layer_norm/scale'.format(prefix)]
+  sublayer.layer_norm.scale.assign(scale)
+
+  wi = state_dict['{}/DenseReluDense/wi/kernel'.format(prefix)]
+  wo = state_dict['{}/DenseReluDense/wo/kernel'.format(prefix)]
+  sublayer.layer.wi.set_weights([wi])
+  sublayer.layer.wo.set_weights([wo])
+  
+scale = state_dict['encoder/final_layer_norm/scale']
+model.encoder.layer_norm.scale.assign(scale)
+
+out = model((_in, _in_len, _out, _out_len))
 print(out)
--- t5/tfive.py
+++ t5/tfive.py
@@ -131,6 +131,65 @@ class attention(tf.keras.layers.Layer):
     attn_output = self.out(attn_output)
     return attn_output
 
+class encdec_attention(tf.keras.layers.Layer):
+  def __init__(self, num_heads=8, *args, **kwargs):
+    super(encdec_attention, self).__init__(*args, **kwargs)
+    self.num_heads = num_heads
+
+  def build(self, input_shape):
+    if isinstance(input_shape, tuple):
+      dim = input_shape[0][-1]
+
+    else:
+      dim = input_shape[-1]
+
+    self.head_dim = dim // self.num_heads
+    
+    self.q = self.add_weight(shape=(512, 512), name="q")
+    self.k = tf.keras.layers.Dense(512, use_bias=False, name="k")
+    self.v = tf.keras.layers.Dense(512, use_bias=False, name="v")
+    self.out = tf.keras.layers.Dense(512, use_bias=False, name="out")
+
+  def call(self, inputs, training=None):
+    if isinstance(inputs, tuple) and len(inputs) == 2:
+      x, rel_bias = inputs
+
+    else:
+      x = inputs
+      rel_bias = None
+
+    q = tf.linalg.matmul(x, self.q)
+    k = self.k(x)
+    v = self.v(x)
+    
+    def reshape(e):
+      e = tf.reshape(e,
+        tf.concat([tf.shape(e)[:2], [self.num_heads, self.head_dim]], 0))
+      e = tf.transpose(e, [0, 2, 1, 3])
+      e = tf.reshape(e,
+        tf.concat([[-1], tf.shape(e)[-2:]], 0))
+      return e
+
+    q = reshape(q)
+    k = reshape(k)
+    v = reshape(v)
+
+    attn_weights = tf.linalg.matmul(q, k, transpose_b=True)
+    if rel_bias is not None:
+      attn_weights += tf.transpose(rel_bias, [0, 2, 1])
+
+    attn_probs = tf.nn.softmax(attn_weights, -1)
+    attn_output = tf.linalg.matmul(attn_probs, v)
+    
+    attn_output = tf.reshape(attn_output,
+      tf.concat([[-1, self.num_heads], tf.shape(attn_output)[-2:]], 0))
+    attn_output = tf.transpose(attn_output, [0, 2, 1, 3])
+    attn_output = tf.reshape(attn_output,
+      tf.concat([tf.shape(attn_output)[:-2], [-1]], 0))
+
+    attn_output = self.out(attn_output)
+    return attn_output
+
 class denserelu(tf.keras.layers.Layer):
   def __init__(self, *args, **kwargs):
     super(denserelu, self).__init__(*args, **kwargs)
@@ -154,17 +213,13 @@ class denserelu(tf.keras.layers.Layer):
 
     return x
 
-class t5(tf.keras.layers.Layer):
+class t5_encoder(tf.keras.layers.Layer):
   def __init__(self, *args, **kwargs):
-    super(t5, self).__init__(*args, **kwargs)
-
+    super(t5_encoder, self).__init__(*args, **kwargs)
     self.bucket_dim = 32
 
   def build(self, input_shape):
-    self.embed = self.add_weight(shape=(32128, 512), name="embed")
     self.rel_bias = self.add_weight(shape=(8, 32), name="rel_bias")
-    #self.sublayer = sublayer(attention)
-    #self.sublayer2 = sublayer(denserelu)
     self.sublayers = [
       sublayer(attention), sublayer(denserelu),
       sublayer(attention), sublayer(denserelu),
@@ -183,8 +238,6 @@ class t5(tf.keras.layers.Layer):
       x = inputs
       x_len = None
 
-    x = tf.gather(self.embed, x)
-
     rel_pos = tf.reshape(tf.range(128), (-1, 1)) - tf.reshape(tf.range(128), (1, -1))
     rp_bucket = rel_pos_bucket(rel_pos)
     rel_bias = tf.gather(self.rel_bias, rp_bucket, axis=1)
@@ -199,4 +252,76 @@ class t5(tf.keras.layers.Layer):
         x = sublayer((x, x_len))
 
     x = self.layer_norm(x)
+    return x
+
+class t5_decoder(tf.keras.layers.Layer):
+  def __init__(self, *args, **kwargs):
+    super(t5_decoder, self).__init__(*args, **kwargs)
+    self.bucket_dim = 32
+
+  def build(self, input_shape):
+    self.rel_bias = self.add_weight(shape=(8, 32), name="rel_bias")
+    self.sublayers = [
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+      sublayer(attention), sublayer(encdec_attention), sublayer(denserelu),
+    ]
+    self.layer_norm = rms_norm()
+  
+  def call(self, inputs, training=None):
+    if isinstance(inputs, tuple) and len(inputs) == 2:
+      x, x_len = inputs
+
+    else:
+      x = inputs
+      x_len = None
+
+    rel_pos = tf.reshape(tf.range(32), (-1, 1)) - tf.reshape(tf.range(32), (1, -1))
+    rp_bucket = rel_pos_bucket(rel_pos)
+    rel_bias = tf.gather(self.rel_bias, rp_bucket, axis=1)
+    if x_len is not None:
+      mask = tf.cast(tf.sequence_mask(x_len, tf.shape(x)[1]), x.dtype)
+      rel_bias += (1. - tf.expand_dims(mask, -1)) * (-1e9) 
+
+    for sublayer in self.sublayers:
+      if sublayer.layer_cls is attention:
+        x = sublayer((x, x_len, rel_bias))
+      else:
+        x = sublayer((x, x_len))
+
+    x = self.layer_norm(x)
+    return x
+
+class t5(tf.keras.layers.Layer):
+  def __init__(self, *args, **kwargs):
+    super(t5, self).__init__(*args, **kwargs)
+
+  def build(self, input_shape):
+    self.embed = self.add_weight(shape=(32128, 512), name="embed")
+    self.encoder = t5_encoder()
+    self.decoder = t5_decoder()
+  
+  def call(self, inputs, training=None):
+    if isinstance(inputs, tuple) and len(inputs) == 4:
+      x, x_len, y, y_len = inputs
+
+    elif isinstance(inputs, tuple) and len(inputs) == 2:
+      x, x_len = inputs
+      y = None; y_len = None
+
+    else:
+      x = inputs
+      x_len = None
+      y = None; y_len = None
+    
+    x = tf.gather(self.embed, x)
+    x = self.encoder((x, x_len))
+
+    if y is not None:
+      y = tf.gather(self.embed, y)
+      y = self.decoder((y, y_len))
+
     return x
